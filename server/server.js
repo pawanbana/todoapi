15221c337abce0bca043bef8bce56c45c2077dc6
Pawan Bana <bana.pawan@gmail.com>
2018-09-16 00:25:20 +0530

    added  id fetch

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express= require('express');
 var bodyparser=require('body-parser');
+const {ObjectID}=require('mongodb');
 
 
 var {mongoose}=require("./db/mongoose.js");
@@ -38,6 +39,27 @@ app.get('/todos',(req,res)=>{
       
    });
    
+
+});
+
+app.get('/todos/:id',(req,res)=>{
+   
+   var id=req.params.id;
+   if(!ObjectID.isValid(id)){
+
+   return res.status(404).send("id is not valid");
+
+   }
+ 
+  Todo.findById(id).then((todo)=>{
+  	if(!todo){
+      return res.status(404).send("data is not present");
+  	}
+        res.send(todo);
+  
+  }).catch((e)=>{
+  	res.status(400).send(e);
+  });
 
 });
 
