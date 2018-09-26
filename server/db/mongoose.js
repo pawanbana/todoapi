3cc4f6d6f6efc338dcd2f8b13f49c003d4f6098f
Pawan Bana <bana.pawan@gmail.com>
2018-09-27 03:12:03 +0530

    change server

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -9,8 +9,8 @@ var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
 
-//mongoose.connect('mongodb://pawan:1908199p@ds261342.mlab.com:61342/nodetodoapidata',{ useNewUrlParser: true });
-mongoose.connect('mongodb://127.0.0.1:27017/TodoApp',{ useNewUrlParser: true });
+mongoose.connect('mongodb://pawan:1908199p@ds261342.mlab.com:61342/nodetodoapidata',{ useNewUrlParser: true });
+//mongoose.connect('mongodb://127.0.0.1:27017/TodoApp',{ useNewUrlParser: true });
 
 
 
